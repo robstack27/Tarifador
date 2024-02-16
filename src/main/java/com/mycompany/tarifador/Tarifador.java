8d8097ac437c0a3e9ea544f781c9827c609d806e
Omar <toapantaomar12@gmail.com>
2024-02-16 14:45:13 -0500

    Proyecto Base

---
+++ src/main/java/com/mycompany/tarifador/Tarifador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarifador;
+
+/**
+ *
+ * @author labctr
+ */
+public class Tarifador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
